65e51705118eced06ebd44b718145651e04f1751
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2026-02-18 19:08:07 +0100

    lock to nixos 25.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # nixpkgs.url = "github:LunNova/nixpkgs/lunnova/rocm-6.4.x";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
